07f00412caf694d16e6d8c3ea88b9686ba5476c4
MJ <mridulajayaraman@honeycomb.io>
2022-09-19 15:17:16 -0700

    terraform format check feedback

--- modules/explore-buildevents-starter-pack/explore-build-durations.tf
+++ modules/explore-buildevents-starter-pack/explore-build-durations.tf
@@ -17,8 +17,8 @@ data "honeycombio_query_specification" "build_times_over_2_min" {
 
   filter {
     column = "duration_ms"
-    op = ">"
-    value = var.ideal_build_duration_ms
+    op     = ">"
+    value  = var.ideal_build_duration_ms
   }
 
   time_range = var.query_time_range
@@ -30,8 +30,8 @@ resource "honeycombio_query" "build_times_over_ideal" {
 }
 
 resource "honeycombio_query_annotation" "build_times_over_ideal_annotation" {
-    dataset     = var.dataset
-    query_id    = honeycombio_query.build_times_over_ideal.id
-    name        = "Which builds are slow?"
-    description = "Explore builds that are taking longer than the preffered ${var.ideal_build_duration_ms} milliseconds"
+  dataset     = var.dataset
+  query_id    = honeycombio_query.build_times_over_ideal.id
+  name        = "Which builds are slow?"
+  description = "Explore builds that are taking longer than the preffered ${var.ideal_build_duration_ms} milliseconds"
 }
--- modules/explore-buildevents-starter-pack/main.tf
+++ modules/explore-buildevents-starter-pack/main.tf
@@ -1,25 +1,25 @@
 terraform {
   required_providers {
     honeycombio = {
-      source = "honeycombio/honeycombio"
+      source  = "honeycombio/honeycombio"
       version = "~> 0.10.0"
     }
   }
 }
 
 resource "honeycombio_board" "buildevents_board" {
-  name        = "Explore Buildevents"
-  style       = "visual"
+  name  = "Explore Buildevents"
+  style = "visual"
 
   query {
-    caption = "Explore build failures"
+    caption             = "Explore build failures"
     query_annotation_id = honeycombio_query_annotation.success_failure_breakdown_annotation.id
-    query_id = honeycombio_query.success_failure_breakdown.id
+    query_id            = honeycombio_query.success_failure_breakdown.id
   }
 
   query {
-    caption = "Slow Builds? Build Times > ${var.ideal_build_duration_ms} milliseconds"
+    caption             = "Slow Builds? Build Times > ${var.ideal_build_duration_ms} milliseconds"
     query_annotation_id = honeycombio_query_annotation.build_times_over_ideal_annotation.id
-    query_id = honeycombio_query.build_times_over_ideal.id
+    query_id            = honeycombio_query.build_times_over_ideal.id
   }
 }
